1c8df9b4c3a11d8f7a17f3709c7b69db511b3008
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-04-04 19:03:30 -0400

    added

--- cypress/e2e/3-Basics/traverse2.cy.js
+++ cypress/e2e/3-Basics/traverse2.cy.js
@@ -21,5 +21,20 @@ describe('traverse methods',function(){
     it('To get parent DOM element of elements, use the .parent() command.',function(){
         cy.get('.disabled').parent().should('have.attr','class','traversal-button-states')
     })
+    it.skip('To get descendant DOM elements of the selector, use the .find() command.',function(){
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.get('#contact_me').find('input[name="first_name"]').type('satya')
+
+    })
+    it.only('To get all of the next sibling DOM elements within elements until another element, use the .nextUntil() command.',function(){
+        cy.get('#coffee').nextUntil('#sugar').should('have.length',3)
+    })
+    it('To get all of the next sibling DOM elements within elements until another element, use the .prevUntil() command.',function(){
+        cy.get('#sugar').prevUntil('#coffee').should('have.length','3')
+        cy.get('#sugar').prevUntil('#coffee').should('be.greaterThan','2')
+    })
+    it.only('To get parents DOM element of elements, use the .parents() command.',function(){
+        cy.get('#milk').parents().should('have.class','thumbnail')
+    })
 
 })
